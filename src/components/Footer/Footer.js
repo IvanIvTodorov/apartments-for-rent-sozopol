4d83674c7f6395cc62f5a13674c0f728fe238fbe
Ivan Todorov <iwan.todorow@gmail.com>
2023-06-27 23:55:12 +0300

    Responsive updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,17 +3,19 @@ import { LangContext } from "../../contexts/Language";
 import { useContext } from "react";
 
 export const Footer = () => {
-    const {lang} = useContext(LangContext);
+    const { lang } = useContext(LangContext);
     if (lang === 'BG') {
         return (
             <footer>
                 <p>
-                <i className="fa-solid fa-phone"></i>ЗА РЕЗЕРВАЦИИ:
-                <img width="25" height="25" src="https://img.icons8.com/fluency/48/bulgaria.png" alt="bulgaria"/>
-                <a href="tel:+359878194897">+359878194841</a>
-                <img width="25" height="25" src="https://img.icons8.com/fluency/48/czech-republic-circular.png" alt="czech-republic-circular"/>
-                <a href="tel:+420608194825">+420608194825</a>
-                <i className="fa-solid fa-phone"></i>
+                    <i className="fa-solid fa-phone"></i>
+                    ЗА РЕЗЕРВАЦИИ: ИВЕЛИН ТОДОРОВ
+                    <img width="25" height="25" src="https://img.icons8.com/fluency/48/bulgaria.png" alt="bulgaria" />
+                    <a href="tel:+359878194897">+359878194841</a>
+                    ,
+                    <img width="25" height="25" src="https://img.icons8.com/fluency/48/czech-republic-circular.png" alt="czech-republic-circular" />
+                    <a href="tel:+420608194825">+420608194825</a>
+                    <i className="fa-solid fa-phone"></i>
                 </p>
             </footer>
         )
@@ -23,16 +25,16 @@ export const Footer = () => {
         return (
             <footer>
                 <p>
-                <i className="fa-solid fa-phone"></i>
-                FOR RESERVATIONS:
-                <img width="25" height="25" src="https://img.icons8.com/fluency/48/bulgaria.png" alt="bulgaria"/>
-                <a href="tel:+359878194897">+359878194841</a>
-                <img width="25" height="25" src="https://img.icons8.com/fluency/48/czech-republic-circular.png" alt="czech-republic-circular"/>
-                <a href="tel:+420608194825">+420608194825</a> 
-                <i className="fa-solid fa-phone"></i>
+                    <i className="fa-solid fa-phone"></i>
+                    FOR RESERVATIONS: IVELIN TODOROV
+                    <img width="25" height="25" src="https://img.icons8.com/fluency/48/bulgaria.png" alt="bulgaria" />
+                    <a href="tel:+359878194897">+359878194841</a>
+                    ,
+                    <img width="25" height="25" src="https://img.icons8.com/fluency/48/czech-republic-circular.png" alt="czech-republic-circular" />
+                    <a href="tel:+420608194825">+420608194825</a>
+                    <i className="fa-solid fa-phone"></i>
                 </p>
             </footer>
         )
     }
-   
 }
